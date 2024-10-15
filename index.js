3c3409378b50a16f38a518b58fffa3c8b86b4b94
Thiago Mesquita <thiagomesquitaitu@gmail.com>
2024-10-15 17:11:46 -0300

    Desafio Herói

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello, world!');
+let heroi = ['Batman', 9500];
+let nivel;
+
+if (heroi[1] <= 1000) {
+  nivel = 'Ferro';
+} else if (heroi[1] <= 2000) {
+  nivel = 'Bronze';
+} else if (heroi[1] <= 5000) {
+  nivel = 'Prata';
+} else if (heroi[1] <= 7000) {
+  nivel = 'Ouro';
+} else if (heroi[1] <= 8000) {
+  nivel = 'Platina';
+} else if (heroi[1] <= 9000) {
+  nivel = 'Ascendente';
+} else {
+  nivel = 'Radiante';
+}
+
+console.log('O Herói de nome ' + heroi[0] + ' está no nível de ' + nivel);
