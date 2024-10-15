665cb5b3de503dcf2064871f6405f059f80e26b5
Thiago Mesquita <thiagomesquitaitu@gmail.com>
2024-10-15 18:15:51 -0300

    interface interativa adicionada

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-let heroi = ['Batman', 9500];
+let heroi = ['', 0];
 let nivel;
 
-if (heroi[1] <= 1000) {
-  nivel = 'Ferro';
-} else if (heroi[1] <= 2000) {
-  nivel = 'Bronze';
-} else if (heroi[1] <= 5000) {
-  nivel = 'Prata';
-} else if (heroi[1] <= 7000) {
-  nivel = 'Ouro';
-} else if (heroi[1] <= 8000) {
-  nivel = 'Platina';
-} else if (heroi[1] <= 9000) {
-  nivel = 'Ascendente';
-} else {
-  nivel = 'Radiante';
-}
+function verificar() {
+  heroi[0] = document.querySelector('#heroi').value;
+  heroi[1] = document.querySelector('#nivel').value;
 
-console.log('O Herói de nome ' + heroi[0] + ' está no nível de ' + nivel);
+  if (heroi[1] <= 1000) {
+    nivel = 'Ferro⚪';
+  } else if (heroi[1] <= 2000) {
+    nivel = 'Bronze🟤';
+  } else if (heroi[1] <= 5000) {
+    nivel = 'Prata🔘';
+  } else if (heroi[1] <= 7000) {
+    nivel = 'Ouro🟡';
+  } else if (heroi[1] <= 8000) {
+    nivel = 'Platina🔵';
+  } else if (heroi[1] <= 9000) {
+    nivel = 'Ascendente🟢';
+  } else if (heroi[1] <= 10000) {
+    nivel = 'Imortal🔴';
+  } else {
+    nivel = 'Radiante🔶';
+  }
+
+  document.querySelector(
+    'section',
+  ).innerHTML = `<h1>O Herói de nome <strong>${heroi[0]}</strong> está no nível de <strong>${nivel}</strong></h1>`;
+
+  document.querySelector('#botao').style.display = 'block';
+}
